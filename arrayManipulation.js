39ac4295bdc544f6bcdd7df5cdd849573811437b
hawamicha <152286703+hawamicha@users.noreply.github.com>
2024-05-19 22:13:34 +0000

    created formatArrayStrings - task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -8,3 +8,21 @@ function processArray(numbers)  {
     });
 }
 
+
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+
+num = processArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+stringArray = ['hello', 'world', 'this', 'is', 'a', 'test'];
+
+formatArrayStrings(stringArray, num);
+
